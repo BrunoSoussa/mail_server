d00bf80b52ab94e677f8202bc0f6d29ecc3108cf
Bruno <brunosousasilvapi@gmail.com>
2025-03-25 15:15:42 -0300

    teste app

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def create_app():
     
     return app
 
-
-app = create_app()
-with app.app_context():
-    db.create_all()
-app.run(debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
